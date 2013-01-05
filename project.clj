f94699de311985391d9e4bc8186f3ddf67f37314
bouzuya <bouzuya@gmail.com>
2013-01-06 08:52:15 +0900

    fixed no response bug. version 0.1.0 -> 0.1.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject tweets-counter "0.1.0"
+(defproject tweets-counter "0.1.1"
   :description "Tweets Counter counts tweets, followers, friends and favorites."
   :url "http://github.com/bouzuya/clj-tweets-counter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [twitter-api "0.6.13"]]
+                 [org.twitter4j/twitter4j-core "2.2.4"]]
   :main tweets-counter.core)
 
--- src/tweets_counter/core.clj
+++ src/tweets_counter/core.clj
@@ -1,7 +1,5 @@
 (ns tweets-counter.core
-  (:require [twitter.oauth :as oauth]
-            [twitter.api.restful :as twitter]
-            [clojure.java.io :as jio]
+  (:require [clojure.java.io :as jio]
             [clojure.string :as str])
   (:gen-class))
 
@@ -23,17 +21,21 @@
   [& args]
   (let [props (props->map (props "my.properties"))
         file (jio/file (props :filename))
-        user (props :username)
-        body (:body (twitter/show-user :params {:screen-name user}))
+        username (props :username)
+        twitter (twitter4j.TwitterFactory/getSingleton)
+        user (.showUser twitter username)
+        tweets-count (.getStatusesCount user)
+        following-count (.getFriendsCount user)
+        followers-count (.getFollowersCount user)
+        favorites-count (.getFavouritesCount user)
         formatter (java.text.SimpleDateFormat. "yyyy-MM-dd")
         now (.getTime (java.util.Calendar/getInstance))
         line (str/join "," [(.format formatter now)
-                            (:statuses_count body)
-                            (:followers_count body)
-                            (:friends_count body)
-                            (:favourites_count body)])]
-    (when body
-      (with-open [w (jio/writer file :append true)]
-        (.write w line)
-        (.newLine w)))))
+                            tweets-count
+                            following-count
+                            followers-count
+                            favorites-count])]
+    (with-open [w (jio/writer file :append true)]
+      (.write w line)
+      (.newLine w))))
 
